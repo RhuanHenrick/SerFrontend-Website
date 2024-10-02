4599be0ec4691a83d0638cdabb100a0025616b41
Rhuan Henrick <rhuanhcr@gmail.com>
2024-10-02 14:20:31 -0300

    Adicionando apenas a pasta desejada

--- aula-7 js/desc.js
+++
@@ -1,9 +0,0 @@
-let price = 800;
-let valorDesconto = 0.25;
-
-function saleStatus(price, valorDesconto) {
-    let desconto = price * valorDesconto;
-    let total = price - desconto;
-    console.log("Valor do produto $ ", price +  ", valor do produto com desconto $ ", total )
-}
-saleStatus(price,valorDesconto);
